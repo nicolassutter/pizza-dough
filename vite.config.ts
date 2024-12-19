18ee8ea7219d65be8b54f9bd8103b399754164ae
nicolassutter <contact.sutter.nicolas@gmail.com>
2024-12-19 14:57:03 +0100

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Icons from 'unplugin-icons/vite'
 import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -13,6 +14,39 @@ export default defineConfig({
       iconCustomizer(_collection, _icon, props) {
         props.width = '1em'
         props.height = '1em'
+      },
+    }),
+    VitePWA({
+      includeAssets: ['pizza.svg', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Cyberdough',
+        short_name: 'Cyberdough',
+        description: 'Pizza dough calculator',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
       },
     }),
   ],
