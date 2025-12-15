3bdb59d630203bf11993a7acd66ff59316540020
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-12-15 17:40:49 +0100

    feat: add dynamic color mode meta tag

--- app/app.vue
+++ app/app.vue
@@ -11,12 +11,37 @@ const title = 'CyberDough - Pizza Dough Calculator'
 const description =
   'Calculate the perfect pizza dough ingredients for your homemade pizzas. Adjust hydration, weight, and quantity to get precise measurements.'
 
+const colorMode = useColorMode()
+
+const metaColorModes = {
+  light: '#ffffff',
+  dark: '#0f172b',
+}
+
 useSeoMeta({
   title,
   description,
   ogTitle: title,
   ogDescription: description,
   twitterCard: 'summary_large_image',
+  themeColor: [
+    {
+      media: '(prefers-color-scheme: light)',
+      content: metaColorModes.light,
+    },
+    {
+      media: '(prefers-color-scheme: dark)',
+      content: metaColorModes.dark,
+    },
+    // reactive meta tag for theme color
+    {
+      content: computed(() => {
+        return colorMode.value === 'dark'
+          ? metaColorModes.dark
+          : metaColorModes.light
+      }),
+    },
+  ],
 })
 
 const { $pwa } = useNuxtApp()
